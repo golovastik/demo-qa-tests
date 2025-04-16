dd2de6a4014d1cf88f5df2714d79b0e24319a54b
golovastik <test@>
2025-04-16 22:29:01 +0300

    added PracticeFormTests 2

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ public class PracticeFormTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
     }
 
     //automation-practice-form
@@ -18,19 +20,23 @@ public class PracticeFormTests {
     @Test
     void inputData() {
         open("/automation-practice-form");
+
         $( "#firstName").setValue("April");
         $("#lastName").setValue("Chocolate");
         $("#userEmail").setValue("chocoapril@mail.com");
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("+375292584");
-        //$("#dateOfBirthInput").setValue("12  2025");
-        $("#subjectsContainer").setValue("add");
-        $("#hobbies-checkbox-1").click();
-        $("#hobbies-checkbox-3").click();
-        //picture
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("May")).click();
+        $(".react-datepicker__year-dropdown-container").$(byText("1903")).click();
+        $(".react-datepicker__month-container").$(byText("18")).click();
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("1.jpg");
         $("#currentAddress").setValue("USA Los Angeles Big street 14 aleha 44/31");
-        //State
-        //City
+        $(". css-1wy0on6").$(byText("NCR")).click();
+        $(". css-1wy0on6").$(byText("Noida")).click();
         $("#submit").click();
 
     }
