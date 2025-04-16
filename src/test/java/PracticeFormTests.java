b544cca25bcabefc99fc52ac6440bdf9c83b095d
golovastik <test@>
2025-04-16 22:48:38 +0300

    added PracticeFormTests 3

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -3,6 +3,7 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -25,7 +26,7 @@ public class PracticeFormTests {
         $("#lastName").setValue("Chocolate");
         $("#userEmail").setValue("chocoapril@mail.com");
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("+375292584");
+        $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-dropdown-container").$(byText("May")).click();
         $(".react-datepicker__year-dropdown-container").$(byText("1903")).click();
@@ -33,11 +34,18 @@ public class PracticeFormTests {
         $("#subjectsInput").setValue("English").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("1.jpg");
+        $("#uploadPicture").uploadFromClasspath("pxfuel.jpg");
         $("#currentAddress").setValue("USA Los Angeles Big street 14 aleha 44/31");
-        $(". css-1wy0on6").$(byText("NCR")).click();
-        $(". css-1wy0on6").$(byText("Noida")).click();
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
         $("#submit").click();
 
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-content").shouldHave(text("April"));
+        $(".modal-content").shouldHave(text("Chocolate"));
+        $(".modal-content").shouldHave(text("chocoapril@mail.com"));
+        $(".modal-content").shouldHave(text("1234567890"));
+        $(".modal-content").shouldHave(text("NCR Noida"));
+        $("#closeLargeModal").click();
     }
 }
