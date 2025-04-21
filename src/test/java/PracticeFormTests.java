3f7d3ce3c8bab52aad673087ae4e56c07a0ac4f5
golovastik <test@>
2025-04-21 22:38:28 +0300

    added PracticeFormTests 4

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -3,10 +3,10 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
@@ -19,27 +19,49 @@ public class PracticeFormTests {
     //automation-practice-form
 
     @Test
-    void inputData() {
+    void inputDataRegistration() {
         open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         $( "#firstName").setValue("April");
         $("#lastName").setValue("Chocolate");
         $("#userEmail").setValue("chocoapril@mail.com");
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("1234567890");
+
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-dropdown-container").$(byText("May")).click();
-        $(".react-datepicker__year-dropdown-container").$(byText("1903")).click();
-        $(".react-datepicker__month-container").$(byText("18")).click();
+        $(".react-datepicker__month-select").selectOption("July");
+//        $(".react-datepicker__month-dropdown-container").selectOptionByValue("6");
+//        $(".react-datepicker__month-dropdown-container").$(byText("May")).click();
+//        $(".react-datepicker__month-dropdown-container").selectOption("July");
+//        $(".react-datepicker__year-dropdown-container").$(byText("1903")).click();
+        $(".react-datepicker__year-select").selectOption("1995");
+//        $(".react-datepicker__month-container").$(byText("18")).click();
+        $(".react-datepicker__day--023:not(.react-datepicker__day--outside-month)").click();
+
+
         $("#subjectsInput").setValue("English").pressEnter();
+
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbiesWrapper").$(byText("Music")).click();
+
         $("#uploadPicture").uploadFromClasspath("pxfuel.jpg");
+
         $("#currentAddress").setValue("USA Los Angeles Big street 14 aleha 44/31");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Noida").pressEnter();
+
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+//        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+//        $("#react-select-4-input").setValue("Noida").pressEnter();
         $("#submit").click();
 
+
+        $(".modal-dialog").should(appear);
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
         $(".modal-content").shouldHave(text("April"));
         $(".modal-content").shouldHave(text("Chocolate"));
