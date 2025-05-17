9eee2f5d5f63a41674b3c03aba8258105678aaae
golovastik <test@>
2025-05-17 15:28:35 +0300

    test data end

--- src/test/java/testElements/ElementOfPage.java
+++ src/test/java/testElements/ElementOfPage.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class ElementsPage {
+public class ElementOfPage {
 
     @BeforeAll
     static void beforeAll() {
